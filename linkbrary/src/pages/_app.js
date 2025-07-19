d3a4308cadb4b1bf8501c563e624a861d24a39d5
DESKTOP-BDRHND2\배혜민 <phsoo1997@daum.net>
2025-07-19 13:28:43 +0900

    [배혜민]feature/common-공통컴포넌트 추가

--- linkbrary/src/pages/_app.js
+++ linkbrary/src/pages/_app.js
@@ -1,3 +1,12 @@
+import { ThemeProvider } from "@emotion/react";
+import GlobalStyle from "../styles/GlobalStyles.jsx";
+import theme from "../styles/theme";
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
