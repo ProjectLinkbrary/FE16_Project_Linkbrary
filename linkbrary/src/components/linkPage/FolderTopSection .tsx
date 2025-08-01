165ef29b7bd082cb0b18b50dfe870359118571eb
Jung, Sumin <gigi3316@naver.com>
2025-08-01 12:43:16 +0900

    [정수민] 링크 공유하기 모달 1차 완료

--- linkbrary/src/components/linkPage/FolderTopSection .tsx
+++ linkbrary/src/components/linkPage/FolderTopSection .tsx
@@ -1,5 +1,6 @@
 /** @jsxImportSource @emotion/react */
 import styled from "@emotion/styled";
+import Image from "next/image";
 
 const FolderTag = styled.div`
   width: 100%;
@@ -15,6 +16,7 @@ const FolderTitle = styled.h2`
 
 const FolderActions = styled.div`
   display: flex;
+  gap: 8px;
 `;
 
 const IconButton = styled.button`
@@ -22,31 +24,41 @@ const IconButton = styled.button`
   border: none;
   padding: 4px;
   cursor: pointer;
+  display: flex;
+  align-items: center;
 `;
-
-const ShareIcon = styled.img``;
-const EditIcon = styled.img``;
-const TrashIcon = styled.img``;
 
 interface FolderTopSectionProps {
   folderTitle?: string;
+  onShareClick: () => void;
 }
 
 export default function FolderTopSection({
   folderTitle,
+  onShareClick,
 }: FolderTopSectionProps) {
   return (
     <FolderTag>
       <FolderTitle>{folderTitle ?? "기본 폴더명"}</FolderTitle>
       <FolderActions>
-        <IconButton type="button">
-          <ShareIcon src="/images/ic_share.svg" alt="공유" />
+        <IconButton type="button" onClick={onShareClick}>
+          <Image src="/images/ic_share.svg" alt="공유" width={24} height={24} />
         </IconButton>
         <IconButton type="button">
-          <EditIcon src="/images/ic_btn.svg" alt="폴더 수정하기" />
+          <Image
+            src="/images/ic_btn.svg"
+            alt="폴더 수정하기"
+            width={24}
+            height={24}
+          />
         </IconButton>
         <IconButton type="button">
-          <TrashIcon src="/images/ic_trash.svg" alt="폴더 삭제" />
+          <Image
+            src="/images/ic_trash.svg"
+            alt="폴더 삭제"
+            width={24}
+            height={24}
+          />
         </IconButton>
       </FolderActions>
     </FolderTag>
